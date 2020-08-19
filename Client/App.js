207cd3b47e7942e9fe9c09b51ea538b424302ed7
ninad bondre <bondre.gunal@gmail.com>
2020-08-19 21:06:27 +0530

    initial commit

---
+++ Client/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+export default class App extends Component {
+  render() {
+    return <div>hello world</div>;
+  }
+}
+ReactDOM.render(<App />, document.getElementById("root"));
